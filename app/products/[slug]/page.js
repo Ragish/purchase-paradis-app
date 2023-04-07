fa80fdb1958c88603d701eae0e29d96140561df2
Ragish <ragish.tandukar@gmail.com>
2023-04-07 13:35:12 +1000

    changes

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -1,36 +1,46 @@
-// // getStaticProps in Next13
-// async function getCharacters() {
-//   return await (await fetch("http://localhost:3000/api/single-product")).json();
-// }
-
-// // getStaticPaths in Next13
-// export async function generateStaticParams() {
-//   const characters = await getCharacters();
-//   console.log("This is a copnsole log", characters);
-
-//   return characters?.results.map((c) => ({
-//     slug: c?.name.replace(/\s+/g, "-").toLowerCase(),
-//   }));
-// }
-
-// export default function Staticpage({ params }) {
-//   return (
-//     <>
-//       <h1>The character name is: {params.slug}</h1>
-//     </>
-//   );
-// }
-
+// app/products/ProductDetailsPage.js
+"use client";
 import React from "react";
 
-const PageProps = {
-  params: {
-    slug: "",
-  },
-};
+async function fetchDetailsPage(slug) {
+  //   console.log(slug);
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/single-product?name=${slug}`
+    );
+    const data = await response.json();
+    //console.log(data);
+    return data;
+  } catch (error) {
+    console.error("Error fetching product details:", error);
+    return null;
+  }
+}
 
-function ProductDetailsPage({ params: { slug } }) {
-  return <div>ProductDetailsPage: {slug}</div>;
+function ProductDetailsPage({ params }) {
+  const slug = params?.slug;
+  const [detailspage, setDetailsPage] = React.useState(null);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const [data] = await fetchDetailsPage(slug); // Destructure the array to get the first element
+      setDetailsPage(data);
+    }
+
+    fetchData();
+  }, [slug]);
+
+  if (!detailspage) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>ProductDetailsPage: {slug}</h1>
+      <p>Name: {detailspage.name}</p>
+      <p>Price: {detailspage.price}</p>
+    </div>
+  );
 }
 
 export default ProductDetailsPage;
