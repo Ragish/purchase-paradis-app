7ebd217e96e42508fad24c84b9cc79b8e607ac8d
Ragish <ragish.tandukar@gmail.com>
2023-04-07 10:39:30 +1000

    changes

--- app/products/ProductListing.js
+++ app/products/ProductListing.js
@@ -3,16 +3,10 @@
 import React, { useState } from "react";
 import styles from "../../styles/productListing.module.css";
 import Image from "next/image";
-// import Link from "next/link";
-
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const ProductListing = ({ products }) => {
   const router = useRouter();
-
-  function handleClick(productName) {
-    router.push(`/products/${productName}`);
-  }
 
   const [loaded, setLoaded] = useState(false);
   return (
@@ -30,21 +24,20 @@ const ProductListing = ({ products }) => {
                 key={product._id}
                 className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3 p-2"
               >
-                <div
-                  className="bg-white rounded-md p-4 h-full flex flex-col"
-                  onClick={() => handleClick(product.name)}
-                >
-                  <Image
-                    src={product.image}
-                    alt={product.name}
-                    className={`w-full h-48 object-cover object-center mb-4 rounded transition-opacity duration-500 ${
-                      loaded ? "opacity-100" : "opacity-0"
-                    }`}
-                    loading="lazy"
-                    onLoad={handleImageLoad}
-                    width={262}
-                    height={192}
-                  />
+                <div className="bg-white rounded-md p-4 h-full flex flex-col">
+                  <link href="/products/${productName}">
+                    <Image
+                      src={product.image}
+                      alt={product.name}
+                      className={`w-full h-48 object-cover object-center mb-4 rounded transition-opacity duration-500 ${
+                        loaded ? "opacity-100" : "opacity-0"
+                      }`}
+                      loading="lazy"
+                      onLoad={handleImageLoad}
+                      width={262}
+                      height={192}
+                    />
+                  </link>
 
                   <div className={styles["product-details"]}>
                     <h3 className="text-xl font-semibold">{product.name}</h3>
